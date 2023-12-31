7076206df04306ccfa976b4462bb32aa4e02d478
ADB <adabraemma@gmail.com>
2023-12-31 20:17:05 +0100

    Improve the UX of the site

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 // AOS swiper library init
 AOS.init();
-
 const navElement = document.querySelector("#nav");
-const linkElement = document.querySelector("#links")
-const exitMenu = document.querySelector("#exit-menu-btn")
-const menuBtn = document.querySelector("#menu-btn")
+const linkElement = document.querySelector("#links");
+const exitMenu = document.querySelector("#exit-menu-btn");
+const menuBtn = document.querySelector("#menu-btn");
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const overlayBg = document.querySelector(".hide");
+
+  if (scrollY > 50) {
+    header.classList.add("headerBg");
+    overlayBg.classList.remove("hidden");
+    overlayBg.classList.add("active");
+  } else {
+    header.classList.remove("headerBg");
+    overlayBg.classList.add("hidden");
+    overlayBg.classList.remove("active");
+  }
+
+  if (scrollY > 1) {
+    exitMenu.classList.add("hidden");
+    menuBtn.classList.remove("hidden");
+    $(navElement).fadeOut();
+  }
+});
 
 document.querySelector("#menu-btn").addEventListener("click", function () {
   // const menuBtn
@@ -28,12 +48,12 @@ function removeClass() {
   if (window.innerWidth > 767) {
     navElement.classList.remove("nav-mobile-view");
     linkElement.classList.remove("links-mobile-view");
-    exitMenu.classList.add("hidden")
-    menuBtn.classList.remove("hidden")
+    exitMenu.classList.add("hidden");
+    menuBtn.classList.remove("hidden");
   }
 }
 
-window.addEventListener("resize", function() {
-  removeClass()
-})
+window.addEventListener("resize", function () {
+  removeClass();
+});
 removeClass();
